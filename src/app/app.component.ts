b096833af006fb83c2ade85da3bc166dbdca4a07
Rohit Kumar Sah <rohitsah710@gmail.com>
2020-10-02 21:04:47 +0530

    The first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newComponent';
+  data: { amount: any, description: any }[] 
+  received: any
+
+  receive(event: { amount: any; description: any; }) {
+    this.received = event;
+    if (this.data) {
+      
+       this.data.push({ amount: event.amount, description: event.description });
+    }
+    else {
+      this.data = [{ amount: event.amount, description: event.description }];
+    }
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TableComponent
   ],
-  imports: [
+  imports: [FormsModule,
     BrowserModule
   ],
   providers: [],
