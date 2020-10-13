7b3c175f45415fe45bed3f491a4920c30c2779a3
Rohit Kumar Sah <rohitsah710@gmail.com>
2020-10-13 14:01:17 +0530

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { TableComponent } from './table/table.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MyService } from './data.service';
+
+
+
+
+const routes : Routes = [
+  // {path : '' , component : AppComponent },
+  {path : 'table' , component : TableComponent },
+  {path : 'form' , component : FormComponent }
+]
 
 @NgModule({
   declarations: [
@@ -13,9 +24,9 @@ import { FormsModule } from '@angular/forms';
     TableComponent
   ],
   imports: [FormsModule,
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [MyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
